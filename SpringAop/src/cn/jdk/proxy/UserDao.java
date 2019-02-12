53afed3195c9d7cc6b8f6fb7ed1442d7f135c2d1
张治政 <zhang15036896990@163.com>
2019-02-12 18:21:00 +0800

    jdk的动态代理

---
+++ SpringAop/src/cn/jdk/proxy/UserDao.java
@@ -0,0 +1,10 @@
+package cn.jdk.proxy;
+/**
+ * dao的接口
+ * @author Administrator
+ *
+ */
+public interface UserDao {
+	public void add();
+	public void update();
+}
---
+++ SpringAop/src/cn/jdk/proxy/UserDaoImpl.java
@@ -0,0 +1,17 @@
+package cn.jdk.proxy;
+
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		System.out.println("添加信息。。。");
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		System.out.println("更新信息。。。");
+	}
+
+}
